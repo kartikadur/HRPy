eb54b2ca0a562a60368ab83f6d95909d0cb44e09
kartikadur <kartikadur@gmail.com>
2016-08-24 23:25:59 +0530

    AI bot building improved code

--- botSavesPrincess.py
+++ botSavesPrincess.py
@@ -1,4 +1,4 @@
-grid = [['-', '-', 'p'], ['-', 'm', '-'], ['-', '-', '-']]
+grid = [['-', 'p', '-'], ['-', 'm', '-'], ['-', '-', '-']]
 n = 3
 
 def displayPathtoPrincess(n,grid):
@@ -6,11 +6,11 @@ def displayPathtoPrincess(n,grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == "p" :
-                py, px = i, j
+                py, px = j, i
             if grid[i][j] == "m" :
-                my, mx = i, j
+                my, mx = j, i
 
-    while px != mx and py != my :
+    while px != mx or py != my :
         # princess above bot
         if px < mx :
             mx -= 1
